21fc66ca89594014dc70e9a9d53659f25f0968e1
Carlos <fnino1999@gmail.com>
2021-05-08 13:38:31 -0500

    Prueba :D

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="estilos.css">
+    <script src="main.js" defer></script>
+    <title>Document</title>
+  </head>
+  <body>
+<!--LIBRO-->
+    <div class="book">
+      <div class="front-cover">
+        <div class="first-half"></div>
+        <div class="second-half"></div>
+      </div>
+      <div class="back-cover"></div>
+    </div>
+<!--MENÚ-->
+    <header class="header">
+      <div class="container">
+      <div class="btn-menu">
+        <label for="btn-menu">☰</label>
+      </div>
+      <div class="logo">
+				<h1>Menú</h1>
+			</div>
+    </header>
+  <input type="checkbox" id="btn-menu">
+  <div class="container-menu">
+    <div class="cont-menu">
+      <nav>
+        <a href="#">Idea</a>
+        <a href="#">Referentes</a>
+        <a href="#">Proceso</a>
+        <a href="#">Proyecto</a>
+        <a href="#">Estatement</a>
+      </nav>
+      <label for="btn-menu">✖️</label>
+    </div>
+  </body>
+</html>
